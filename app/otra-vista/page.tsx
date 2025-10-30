7ee4179f58ced1868e465c51c00c06833b375995
vanesa <vanesa.juarez.paris@gmail.com>
2025-10-30 12:50:50 +0100

    add basic nextjs app

--- app/otra-vista/page.tsx
+++ app/otra-vista/page.tsx
@@ -1,20 +1,20 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function OtraVista() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center gap-8 py-32 px-16 bg-white dark:bg-black">
         <h1 className="text-4xl font-bold text-black dark:text-zinc-50">
-          Página Principal
+          Otra Vista
         </h1>
         <p className="text-lg text-zinc-600 dark:text-zinc-400">
-          Bienvenido al home de la aplicación
+          Has navegado a otra vista de la aplicación
         </p>
         <Link
-          href="/otra-vista"
+          href="/"
           className="flex h-12 items-center justify-center rounded-full bg-black px-8 text-white transition-colors hover:bg-zinc-800 dark:bg-white dark:text-black dark:hover:bg-zinc-200"
         >
-          Ir a otra vista
+          Volver al home
         </Link>
       </main>
     </div>
